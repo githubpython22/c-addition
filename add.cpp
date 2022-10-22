7162ad842d88d62f32a08679081854024af8031e
DeepakGithub22 <104898450+DeepakGithub22@users.noreply.github.com>
2022-10-22 12:24:27 +0530

    add x1 and x2

--- add.cpp
+++ add.cpp
@@ -13,7 +13,8 @@ int main() {
         x1 = (-b + sqrt(discriminant)) / (2*a);
         x2 = (-b - sqrt(discriminant)) / (2*a);
         cout << "Roots are real and different." << endl;
-
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
     }
     
     else if (discriminant == 0) {
